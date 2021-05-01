bb8be2fc6e862f4b60be0a86058bb2bc505aec38
michaloumen <michelle.lmendonca@gmail.com>
2021-05-01 16:32:36 -0300

    crud com algumas autenticaçoes

---
+++ models/client.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+var validator = require("email-validator");
+
+var clientSchema = new mongoose.Schema({
+    fullName:{
+        type:String,
+        required: 'This field is required'
+    },
+    email:{
+        type:String
+    },
+    cpf:{
+        type:String,
+        required: true
+    },
+})
+
+// custom validation for email
+clientSchema.path('email').validate((val) => {
+    return validator.validate(val);
+},'Invalid Email');
+
+mongoose.model('Client',clientSchema);
